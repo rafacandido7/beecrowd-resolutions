29ec99e053af64fb7b3ac561ee4c387a44cb74fb
Rafael Candido <rafacandido0708@gmail.com>
2023-09-27 22:08:24 -0300

    feat: 1002

---
+++ 1002/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  double r;
+  double PI = 3.14159;
+  cin >> r;
+
+  double res = (r*r*PI);
+
+  printf("A=%0.4f\n", res);
+
+  return 0;
+}
